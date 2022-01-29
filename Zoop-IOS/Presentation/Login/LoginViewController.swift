cd89b76daa5f9c442069fd053c26d87012cf7954
zaezero97 <xornjs1423@gmail.com>
2022-01-30 07:06:58 +0900

    POD 오류 해결

--- Zoop-IOS/Presentation/Login/LoginViewController.swift
+++ Zoop-IOS/Presentation/Login/LoginViewController.swift
@@ -5,3 +5,22 @@
 //  Created by 재영신 on 2022/01/30.
 //
 
+import UIKit
+import SnapKit
+import
+final class LoginViewController: UIViewController {
+    private lazy var <#variable#>: <#Type#> = {
+        
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
+private extension LoginViewController {
+    func configureUI() {
+        
+    }
+}
