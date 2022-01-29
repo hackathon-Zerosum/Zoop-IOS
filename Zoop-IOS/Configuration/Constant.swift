e550741fc68d6cc1e14249fec3a6bb56a28d4dec
zaezero97 <xornjs1423@gmail.com>
2022-01-29 13:41:18 +0900

    템플릿 생성

---
+++ Zoop-IOS/Configuration/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Zoop-IOS
+//
+//  Created by 재영신 on 2022/01/29.
+//
+
+import Alamofire
+
+struct Constant {
+    static let BASE_URL = "URL 주소를 입력해주세요"
+    static let KOBIS_BASE_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest"
+    
+    //
+}
